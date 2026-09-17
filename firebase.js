b6f0f1ea6e2005a0b9ea25f5293261aaad08080e
champwords <phacchhee@gmail.com>
2026-09-17 18:24:54 +0545

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-// Firebase connection placeholder. Add your Firebase web app config here.
-// Never put Admin SDK credentials or service-account keys in browser code.
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
-};
